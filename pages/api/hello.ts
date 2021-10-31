32ec62d7f67ec75347682ba441c341961c68327b
kaznak <kaznak.at.work@gmail.com>
2021-10-31 14:55:43 +0900

    pages/api/hello.ts: rfct: type annotations

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiHandler } from 'next'
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+interface NameApiResponse {
+  name: string
+}
+
+const handler: NextApiHandler<NameApiResponse> = (_, res) => {
   res.status(200).json({ name: 'John Doe' })
 }
 
